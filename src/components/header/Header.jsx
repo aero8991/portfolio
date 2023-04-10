0fd038f2a23350f3faefa446312b8d0223734242
Dan Rossano <dannyboy.rossano@gmail.com>
2023-04-09 23:16:08 -0700

    updated main pic

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import "./header.css"
 import CTA from './CTA'
-import MainIMG from '../../assets/me.png'
+import MainIMG from '../../assets/dan_pic.png'
 import HeaderSocials from './HeaderSocials'
 import {FaArrowCircleRight} from 'react-icons/fa'
 
